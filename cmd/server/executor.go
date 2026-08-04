ae224dc006d64ec2ec98d14677bfc79efed10ab0
Si33d <sidkumbhar1703@gmail.com>
2026-08-04 16:24:32 +0530

    level1

--- cmd/server/executor.go
+++ cmd/server/executor.go
@@ -7,12 +7,19 @@ import (
 
 func Execute(cmd *Command) ([]byte, error) {
 	str := strings.ToUpper(cmd.Name)
-	
-	switch str{
+
+	switch str {
 	case "PING":
-			return []byte("+PONG\r\n"),nil
+		if len(cmd.Args) == 0 {
+			return []byte("+PONG\r\n"), nil
+		}
+		if len(cmd.Args) == 1 {
+			msg := cmd.Args[0]
+			return []byte(fmt.Sprintf("$%d\r\n%s\r\n", len(msg), msg)), nil
+		}
+		return []byte("-ERR Wrong no.of arguments"), nil
 	default:
-		return nil,fmt.Errorf("Unknown Command",cmd.Name)
+		return nil, fmt.Errorf("Unknown Command %s", cmd.Name)
 	}
 
 }
