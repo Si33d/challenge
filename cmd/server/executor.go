42fdc085a071ce77301b3fabc7ac6643d4558b41
Si33d <sidkumbhar1703@gmail.com>
2026-08-05 22:55:05 +0530

    get set store updated as per expiry

--- cmd/server/executor.go
+++ cmd/server/executor.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func Execute(cmd *Command) ([]byte, error) {
@@ -20,30 +22,43 @@ func Execute(cmd *Command) ([]byte, error) {
 		return []byte("-ERR Wrong no.of arguments"), nil
 
 	case "SET":
-		if len(cmd.Args)!=2{
-			return []byte("-ERR Wrong no. of arguments for SET"),nil
+		if len(cmd.Args) == 2 {
+			store.Set(cmd.Args[0], cmd.Args[1], nil)
+			return []byte("+OK\r\n"), nil
 		}
-		store.Set(cmd.Args[0],cmd.Args[1])
-		return []byte("+OK\r\n"),nil
-		
+		if len(cmd.Args) == 4 {
+			if strings.ToUpper(cmd.Args[2]) != "EX" {
+				return []byte("-ERR Syntax error\r\n"), nil
+			}
+			t, err := strconv.Atoi(cmd.Args[3])
+			if err != nil {
+				return []byte("-ERR Invalid expire time\r\n"), nil
+			}
+
+			expiryTime := time.Now().Add(time.Duration(t) * time.Second)
+
+			store.Set(cmd.Args[0], cmd.Args[1], &expiryTime)
+			return []byte("+OK\r\n"), nil
+		}
+		return []byte("-ERR wrong number of arguments for 'SET'\r\n"), nil
 
 	case "GET":
-		if len(cmd.Args)!=1{
-			return []byte("-ERR Wrong no. of arguments for Get"),nil
+		if len(cmd.Args) != 1 {
+			return []byte("-ERR Wrong no. of arguments for Get"), nil
 		}
-		value,ok:=store.Get(cmd.Args[0])
-		if !ok{
-			return []byte("$-1\r\n"),nil
+		value, ok := store.Get(cmd.Args[0])
+		if !ok {
+			return []byte("$-1\r\n"), nil
 		}
 		return []byte(fmt.Sprintf("$%d\r\n%s\r\n", len(value), value)), nil
-	
+
 	case "DEL":
-		if len(cmd.Args)==0{
-			return []byte("-ERR Wrong no. of arguments for 'DEL'\r\n"),nil
+		if len(cmd.Args) == 0 {
+			return []byte("-ERR Wrong no. of arguments for 'DEL'\r\n"), nil
 		}
-		count:=store.Delete(cmd.Args...)
-		return []byte(fmt.Sprintf(":%d\r\n",count)),nil
-	
+		count := store.Delete(cmd.Args...)
+		return []byte(fmt.Sprintf(":%d\r\n", count)), nil
+
 	case "EXISTS":
 		if len(cmd.Args) != 1 {
 			return []byte("-ERR wrong number of arguments for 'EXISTS'\r\n"), nil
@@ -53,8 +68,21 @@ func Execute(cmd *Command) ([]byte, error) {
 		}
 		return []byte(":0\r\n"), nil
 
+	case "EXPIRE":
+		if len(cmd.Args) != 2 {
+			return []byte("-ERR wrong number of arguments for EXPIRE\r\n"), nil
+		}
+		seconds, err := strconv.Atoi(cmd.Args[1])
+		if err != nil {
+			return []byte("-ERR invalid expire tiem \r\n"), nil
+		}
+		ok := store.Expire(cmd.Args[0], seconds)
+		if ok {
+			return []byte(":1\r\n"), nil
+		}
+		return []byte(":0\r\n"), nil
 	default:
-		return nil, fmt.Errorf("-ERR Unknown Command %s", cmd.Name)
+		return []byte(fmt.Sprintf("-ERR unknown command '%s'\r\n", cmd.Name)), nil
 	}
 
 }
--- cmd/server/store.go
+++ cmd/server/store.go
@@ -1,9 +1,13 @@
 package main
 
-import "sync"
+import (
+	"sync"
+	"time"
+)
 
 type Store struct {
 	data map[string]string
+	expiry map[string]time.Time
 }
 
 var mu sync.RWMutex
@@ -13,22 +17,40 @@ var store = NewStore() //level3 done here only
 func NewStore() *Store {
 	return &Store{
 		data: make(map[string]string),
+		expiry: make(map[string]time.Time),
 	}
 }
 
-func (s *Store) Set(key, value string) {
+func (s *Store) Set(key, value string,expiry *time.Time) {
 	mu.Lock()
 	defer mu.Unlock()
 	s.data[key] = value
 
+	if expiry!=nil{
+		s.expiry[key]=*expiry
+	}else{
+		delete(s.expiry,key)
+	}
 }
 
 func (s *Store) Get(key string) (string, bool) {
 	mu.RLock()
 	defer mu.RUnlock()
-	
+
 	value, ok := s.data[key]
-	return value, ok
+	if !ok{
+		return "",false
+	}
+	exptime,isexpire:=s.expiry[key]
+	if isexpire{
+		if time.Now().After(exptime){
+			delete(s.data,key)
+			delete(s.expiry,key)
+
+			return "",false
+		}
+	}
+	return value,true
 }
 
 func (s *Store) Delete(keys ...string) int {
@@ -51,4 +73,16 @@ func (s *Store) Exists(key string) bool {
 	defer mu.RUnlock()
 	_, ok := s.data[key]
 	return ok
+}
+
+func(s *Store)Expire(key string,seconds int)bool{
+	mu.Lock()
+	defer mu.Unlock()
+
+	_,ok:=s.data[key]
+	if !ok{
+		return false
+	}
+	s.expiry[key]=time.Now().Add(time.Duration(seconds)*time.Second)
+	return true
 }
